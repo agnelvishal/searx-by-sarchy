b469898ef1990e30ae663d5d246edbf2a621975b
root <root@searx-debian.localdomain>
2019-06-22 15:22:38 +0000

    all

--- searx/plugins/open_results_on_new_tab.py
+++ searx/plugins/open_results_on_new_tab.py
@@ -19,7 +19,7 @@ name = gettext('Open result links on new browser tabs')
 description = gettext('Results are opened in the same window by default. '
                       'This plugin overwrites the default behaviour to open links on new tabs/windows. '
                       '(JavaScript required)')
-default_on = False
+default_on = True
 preference_section = 'ui'
 
 js_dependencies = ('plugins/js/open_results_on_new_tab.js',)
